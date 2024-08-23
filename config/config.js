da7129b4ac2141560be4c071787eb15987581116
edsonnaza <edsonnaza@gmail.com>
2024-08-22 21:43:35 -0400

    add: Configuration CI files with Docker

--- config/config.js
+++ config/config.js
@@ -1,25 +1,23 @@
-require('dotenv').config();
-
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || 'myuser',
+    password: process.env.DB_PASSWORD || 'mypassword',
+    database: process.env.DB_NAME || 'aranto',
+    host: process.env.DB_HOST || 'localhost', // Asegúrate de que esto sea correcto
     dialect: 'postgres',
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || 'myuser',
+    password: process.env.DB_PASSWORD || 'mypassword',
+    database: process.env.DB_NAME || 'aranto_test',
+    host: process.env.DB_HOST || 'localhost', // Asegúrate de que esto sea correcto
     dialect: 'postgres',
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || 'myuser',
+    password: process.env.DB_PASSWORD || 'mypassword',
+    database: process.env.DB_NAME || 'aranto',
+    host: process.env.DB_HOST || 'localhost', // Asegúrate de que esto sea correcto
     dialect: 'postgres',
   },
 };
