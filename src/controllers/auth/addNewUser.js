147a3f2001babcbef8721f9927268ad2c981ad4a
edsonnaza <edsonnaza@gmail.com>
2024-08-23 21:12:58 -0400

    add: Return json format response

--- src/controllers/auth/addNewUser.js
+++ src/controllers/auth/addNewUser.js
@@ -4,22 +4,47 @@ const bcrypt = require('bcrypt');
 const addNewUser = async (req, res) => {
   const { email, password, user_name, user_lastname } = req.body;
 
-  if (!email || !password || !user_name || !user_lastname) {
-    return res.status(400).json({
-      error: 'Empty fields',
-      details: {
-        email: req.body.email,
-        password: password,
-        name: user_name,
-        lastname: user_lastname
-      }
-    });
-  }
+  // if (!email || !password || !user_name || !user_lastname) {
+  //   return res.status(400).json({
+  //     error: 'Campos vacíos',
+  //     details: {
+  //       email: req.body.email,
+  //       password: password,
+  //       name: user_name,
+  //       lastname: user_lastname
+  //     }
+  //   });
+
+    let emptyFields = {};
+
+if (!req.body.email) {
+  emptyFields.email = 'Email is required';
+}
+if (!password) {
+  emptyFields.password = 'Password is required';
+}
+if (!user_name) {
+  emptyFields.name = 'Name is required';
+}
+if (!user_lastname) {
+  emptyFields.lastname = 'Lastname is required';
+}
+
+if (Object.keys(emptyFields).length > 0) {
+  return res.status(400).json({
+    error: 'Empty fields',
+    details: emptyFields
+  });
+}
+
+// Continue with the rest of your logic if there are no empty fields
+
+  
 
   try {
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-    if (!hashedPassword) res.status(500).res(`Error with hashed password ${hashedPassword}`);
+    if (!hashedPassword) res.status(500).json({'error':`Error with hashed password ${hashedPassword}`});
 
     const user = await Users.create({
      // id: null,
